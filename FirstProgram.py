4183de6b801078c617b1e232a8cc2e774cade657
Gaurang Parante <gaurangparante2299@gmail.com>
2025-11-26 11:36:01 +0530

    First Push

---
+++ FirstProgram.py
@@ -0,0 +1,24 @@
+print("Hello World!!!")
+
+name = "Gaurang"
+age = 27
+price = 25.99
+old = False
+none = None
+
+print(name)
+print(age)
+print(price)
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(none))
+
+# SUM program
+a = 2
+b = 3
+sum = a + b
+print("The sum of a and b is",sum)
+
