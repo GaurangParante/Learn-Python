9703154270d5c40446a05042e41d78324e3edd0e
Gaurang Parante <gaurangparante2299@gmail.com>
2025-12-02 17:52:11 +0530

    Chapter 2 started

---
+++ Chap2/lecture_two.py
@@ -0,0 +1,30 @@
+# str1 = "This is a string."
+# str2 = 'Gaurang'
+# str3 = """This is also string."""
+
+# str1 = "This is a string.\twe are creating it in python."
+# print(str1)
+
+# a = "Gaurang"
+# len_a = len(a)
+# print(len_a)
+
+# b = "Parante"
+# len_b = len(b)
+# print(len_b)
+# final_str = a + " " + b
+
+# print(final_str)
+# print(len(final_str))
+
+
+# str1 = "GAURANG_PARANTE"
+# ch0 = str1[0]
+# ch3 = str1[3]
+# print(ch3)
+
+str1 = "GAURANG_PARANTE"
+print(str1[2:8])
+print(str1[:4]) #[0:4]
+print(str1[8:]) #[8:len(str1)]
+print(str1[4:len(str1)])
