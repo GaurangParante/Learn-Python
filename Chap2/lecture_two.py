c0079bcdc531221835aeb289ac8bd13b02ca11f2
Gaurang Parante <gaurangparante2299@gmail.com>
2025-12-02 23:38:56 +0530

    Chapter 2 complated

--- Chap2/lecture_two.py
+++ Chap2/lecture_two.py
@@ -1,10 +1,15 @@
+#Strings
 # str1 = "This is a string."
 # str2 = 'Gaurang'
 # str3 = """This is also string."""
 
+
+#escape sequence characters (Formatting)
 # str1 = "This is a string.\twe are creating it in python."
 # print(str1)
 
+
+#Opration On String
 # a = "Gaurang"
 # len_a = len(a)
 # print(len_a)
@@ -18,13 +23,76 @@
 # print(len(final_str))
 
 
+#Indexing
 # str1 = "GAURANG_PARANTE"
 # ch0 = str1[0]
 # ch3 = str1[3]
 # print(ch3)
 
-str1 = "GAURANG_PARANTE"
-print(str1[2:8])
-print(str1[:4]) #[0:4]
-print(str1[8:]) #[8:len(str1)]
-print(str1[4:len(str1)])
+#Slicing
+# str1 = "GAURANG_PARANTE"
+# print(str1[2:8])
+# print(str1[:4]) #[0:4]
+# print(str1[8:]) #[8:len(str1)]
+# print(str1[4:len(str1)])
+
+
+#Nagitive Indexing
+# str1 = "Apple"
+# print(str1[-3:-1])
+
+
+#String Functions
+# str1 = "i am learning python"
+# print(str1.endswith("on"))
+# print(str1.endswith("app"))
+# print(str1.capitalize())
+# print(str1.replace("python","JS"))
+# print(str1.find("o"))
+# print(str1.count("a"))
+
+#Conditional Statements
+# age = 27
+# if(age >= 18):
+#     print("Can vote & apply for license") #Tab (4 spaces) its called indentation
+
+# light = "yellow"
+# if(light == "green"):
+#     print("Go")
+# elif(light == "red"):
+#     print("Stop")
+# elif(light == "yellow"):
+#     print("Look")
+
+# light = "pink"
+# if(light == "green"):
+#     print("Go")
+# elif(light == "red"):
+#     print("Stop")
+# elif(light == "yellow"):
+#     print("Look")
+# else:
+#     print("Light is broken")
+
+
+# marks = int(input("Enter your marks: "))
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
+
+# print("Your grade is",grade)
+
+age = 95
+if(age >= 18):
+    if(age >= 80):
+        print("Can not drive")
+    else:
+        print("Can drive")
+else:
+    print("Can not drive")
---
+++ Chap2/practice_lec_2.py
@@ -0,0 +1,37 @@
+# Program 1
+# name = input("Enter your name: ")
+# print("Length of your name is :",len(name))
+
+# Program 2
+# str1 = "Thi$ i$ new $tring"
+# print("$ occurred",str1.count("$"),"times in string")
+
+# Program 3
+# num = int(input("Enter number: "))
+
+# if(num % 2 == 0):
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# Program 4
+# a = int(input("Enter value of a: "))
+# b = int(input("Enter value of b: "))
+# c = int(input("Enter value of c: "))
+
+# if(a > b and a > c):
+#     largest = a
+# elif(b > c):
+#     largest = b
+# else:
+#     largest = c
+
+# print("Largest number is:",largest)
+
+# Program 5
+num = int(input("Enter number: "))
+
+if(num % 7 == 0):
+    print("Yes the number is multiple of 7")
+else:
+    print("No the number is not multiple of 7")
