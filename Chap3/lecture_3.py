1293222e6c1ab7499c610a49b0db680ab8cc99c5
Gaurang Parante <gaurangparante2299@gmail.com>
2025-12-03 18:00:34 +0530

    Changes DOne

---
+++ Chap3/lecture_3.py
@@ -0,0 +1,24 @@
+# marks1 = 94.4
+# marks2 = 87.5
+# marks3 = 35.2
+# marks4 = 45.1
+# marks5 = 99.8
+
+# marks = [94.4, 87.5, 35.2, 45.1, 99.8]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# student = ["Karan",95.5,17,"Ahmedabad"]
+# print(student)
+# student[0] = "Gaurang"
+# print(student)
+
+marks = [94.4, 87.5, 35.2, 45.1, 99.8]
+
+print(marks[1:4])
+print(marks[:4])
+print(marks[1:])
+print(marks[-3:-1])
