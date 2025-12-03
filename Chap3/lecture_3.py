c50794ad5da907246fa1ea3bf6d570851d631776
Gaurang Parante <gaurangparante2299@gmail.com>
2025-12-03 23:13:45 +0530

    Chap 3 complated

--- Chap3/lecture_3.py
+++ Chap3/lecture_3.py
@@ -16,9 +16,32 @@
 # student[0] = "Gaurang"
 # print(student)
 
-marks = [94.4, 87.5, 35.2, 45.1, 99.8]
+# marks = [94.4, 87.5, 35.2, 45.1, 99.8]
 
-print(marks[1:4])
-print(marks[:4])
-print(marks[1:])
-print(marks[-3:-1])
+# print(marks[1:4])
+# print(marks[:4])
+# print(marks[1:])
+# print(marks[-3:-1])
+
+# list = [2,1,3,1]
+# list.append(4)
+# print(list.sort()) # Its returning None
+# list.sort(reverse=True)
+# list.reverse()
+# list.insert(1,6)
+# list.remove(1)
+# list.pop(2)
+# print(list)
+
+tup = (87,98,36,86,25,36)
+# print(type(tup))
+# print(tup[0])
+# print(tup[2])
+
+# tup[0] = 5 # Not Allowed
+
+# tup = () # valid tupple
+# tup = (1,) # If you want to just put single value in tupple add coma with it else its not considder tupple
+
+print(tup.index(98))
+print(tup.count(36))
---
+++ Chap3/practice_lec_3.py
@@ -0,0 +1,31 @@
+#Program 1
+# list = []
+# a = input("Enter first movie name: ")
+# list.append(a)
+# b = input("Enter second movie name: ")
+# list.append(b)
+# c = input("Enter third movie name: ")
+# list.append(c)
+
+# print(list)
+
+#Program 2
+# list = [1,2,3,2,1]
+
+# list2 = list.copy()
+# list2.reverse()
+
+# if(list == list2):
+#     print("Its pelindrome")
+# else:
+#     print("Its not pelindrome")
+
+#Program 3
+# tup = ("C","D","A","A","B","B","A")
+
+# print(tup.count("A"))
+
+#Program 4
+list = ["C","D","A","A","B","B","A"]
+list.sort()
+print(list)
