a510f00a58853ab3cb46f691fa99398e65df5beb
Gaurang Parante <gaurangparante2299@gmail.com>
2025-12-04 17:17:39 +0530

    Changes done

---
+++ Chap4/lecture_4.py
@@ -0,0 +1,13 @@
+info = {
+    "key":"value",
+    "name":"Gaurang",
+    "subjects":["Python","js","node"],
+    "topic":("dict","set"),
+    "age":35,
+    "is_adult":True,
+    "marks":98.2,
+
+}
+
+print(info)
+print(type(info))
