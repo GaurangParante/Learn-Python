d0ccf9aadbb44429aaaa376993f1f0446d3e5b5d
Gaurang Parante <gaurangparante2299@gmail.com>
2025-12-08 14:24:46 +0530

    break and continue

--- Chap5/lecture_5.py
+++ Chap5/lecture_5.py
@@ -8,7 +8,34 @@
 
 # print(count)
 
-i = 5
-while i > 0:
+# i = 5
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+
+# tupel = (1,2,9,16,25,36,49,64,81,100)
+# n = 9
+# i = 0
+# while i < len(tupel):
+#     if(n == tupel[i]):
+#         print("Found at index:",tupel[i])
+#         break
+#     else:
+#         print('Finding...')
+#     i += 1
+
+i = 1
+while i <= 5:
+    if i == 3:
+        i += 1
+        continue 
     print(i)
-    i -= 1
+    i += 1
+print("End of loop")
