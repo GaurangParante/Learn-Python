1ffc655f0677b3c10b9d1ea2805dc508a2288af4
Gaurang Parante <gaurangparante2299@gmail.com>
2026-01-01 00:05:59 +0530

    chap 8

--- Chap8/lecture_8.py
+++ Chap8/lecture_8.py
@@ -130,14 +130,177 @@
 # # print(acc1.__acc_pass)
 # acc1.reset_pass()
 
-class Person:
-    __name = "anonymous"
+# class Person:
+#     __name = "anonymous"
 
-    def __hello(self):
-        print("Hello")
+#     def __hello(self):
+#         print("Hello")
 
-    def welcome(self):
-        self.__hello()
+#     def welcome(self):
+#         self.__hello()
 
-p1 = Person()
-p1.welcome()
+# p1 = Person()
+# p1.welcome()
+
+## Inheritance
+# Single Inheritance
+
+# class Car:
+#     color = "black"
+#     @staticmethod
+#     def start():
+#         print("Car started...")
+
+#     @staticmethod
+#     def stop():
+#         print("Car stopped...")
+
+# class ToyotaCar(Car):
+#     def __init__(self,name):
+#         self.name = name
+
+
+# car1 = ToyotaCar("Fortuner")
+# car2 = ToyotaCar("Prius")
+# print(car2.color)
+# car2.start()
+
+# Multi-level Inheritance
+
+# class Car:
+#     @staticmethod
+#     def start():
+#         print("Car started...")
+
+#     @staticmethod
+#     def stop():
+#         print("Car Stopped...")
+
+# class ToyotaCar(Car):
+#     def __init__(self,brand):
+#         self.brand = brand
+
+# class Fortuner(ToyotaCar):
+#     def __init__(self,type):
+#         self.type = type
+
+
+# car1 = Fortuner("Disel")
+# car1.start()
+
+# Multiple Inheritance
+
+# class A:
+#     varA = "Welcome class A"
+
+# class B:
+#     varB = "Welcome class B"
+
+# class C(A,B):
+#     varC = "Welcome class C"
+
+# c1 = C()
+# print(c1.varA)
+# print(c1.varB)
+# print(c1.varC)
+
+## Super Methods
+
+# class Car:
+#     def __init__(self,type):
+#         self.type = type
+
+#     @staticmethod
+#     def start():
+#         print("Car Started...")
+
+#     @staticmethod
+#     def stop():
+#         print("Car Stopped...")
+
+# class ToyotaCar(Car):
+#     def __init__(self,name,type):
+#         super().__init__(type)
+#         self.name = name
+#         super().start()
+
+# c1 = ToyotaCar("prius","electric")
+
+# print(c1.type)
+
+## Class Method
+
+# class Person:
+#     name = "Anonymous"
+
+#     # def changeName(self,name):
+#     #     self.__class__.name = name
+#     #     Person.name = name
+#     #     self.name = name
+
+#     @classmethod
+#     def changeName(cls,name):
+#         cls.name = name
+
+# p1 = Person()
+# p1.changeName("Rahul Kumar")
+# print(p1.name)
+# print(Person.name)
+
+# Property
+
+# class Student:
+#     def __init__(self,phy,chem,math):
+#         self.phy = phy
+#         self.chem = chem
+#         self.math = math
+#         # self.percentage = str((self.phy+self.math+self.chem)/3) + "%"
+
+#     # def calcPercentage(self):
+#     #     return str((self.phy+self.math+self.chem)/3) + "%"
+
+#     @property
+#     def percentage(self):
+#         return str((self.phy+self.math+self.chem)/3) + "%"
+
+# s1 = Student(98,97,99)
+
+# print(s1.percentage)
+# s1.phy = 86
+# print(s1.percentage)
+
+## Polymorphism : Operator Overloading
+
+# print(1 + 2) #add
+# print(type(1))
+
+# print("Gaurang" + "Parante") # concate
+# print(type("Gaurang"))
+
+# print([1,2,3] + [4,5,6]) # Merge
+# print(type([1,2,3]))
+
+
+class Complex:
+    def __init__(self,real,img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+       print(self.real ,"i" , self.img , "j")
+    
+    def __add__(self,num2):
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal,newImg)
+    
+    def __sub__(self,num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
+        return Complex(newReal,newImg)
+    
+
+num1 = Complex(1,4)
+num2 = Complex(3,8)
+num3 = num1 - num2
+num3.showNumber()
